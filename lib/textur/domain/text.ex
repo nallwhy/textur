72e203170c7916e3cdd40ad8c93c84b85128bafe
Jinkyou Son <nallwhy@gmail.com>
2025-01-09 19:52:29 +0900

    Update fields of D.Text

--- lib/textur/domain/text.ex
+++ lib/textur/domain/text.ex
@@ -8,15 +8,21 @@ defmodule Textur.Domain.Text do
     attribute :title, :string, allow_nil?: true
     attribute :body, :string, allow_nil?: false
     attribute :password, :string, allow_nil?: true
-    attribute :public, :boolean, default: true, allow_nil?: false
+    attribute :private, :boolean, default: false, allow_nil?: false
+    attribute :private_password, :string, allow_nil?: true
 
     create_timestamp :created_at, type: :utc_datetime_usec
   end
 
   actions do
     create :create do
-      accept [:title, :body, :password, :public]
+      accept [:title, :body, :password, :private]
     end
+  end
+
+  validations do
+    validate attributes_present([:private_password]), where: attribute_equals(:private, true)
+    validate attribute_equals(:private, true), where: attributes_present([:private_password])
   end
 
   sqlite do
